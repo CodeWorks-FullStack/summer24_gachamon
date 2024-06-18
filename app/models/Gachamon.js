60dbb7804c1cc663887428115b0148a660deef1e
Jeremy Fowler <jembyfowler@gmail.com>
2024-06-18 10:40:40 -0600

    stubbed out html template for active

--- app/models/Gachamon.js
+++ app/models/Gachamon.js
@@ -14,9 +14,21 @@ export class Gachamon {
     return `
      <div class="col-2">
         <div onclick="app.GachamonsController.setActiveGachamon('${this.name}')" class="text-center" title="See details for ${this.name}" role="button">
-          <p class="display-1">${this.icon}</p>
+          <p class="display-2">${this.icon}</p>
         </div>
       </div>
+    `
+  }
+
+  get activeHTMLTemplate() {
+    return `
+     <div class="col-12">
+      <div class="text-center">
+        <p class="fs-1">Petey</p>
+        <p class="fs-2">Common</p>
+        <img src="https://em-content.zobj.net/source/microsoft-teams/363/peacock_1f99a.png" alt="Picture of Petey">
+      </div>
+     </div>
     `
   }
 }
