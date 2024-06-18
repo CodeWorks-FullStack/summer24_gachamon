7e6d5750febe428ad6e36780143e2235a55e9e63
Jeremy Fowler <jembyfowler@gmail.com>
2024-06-18 09:28:03 -0600

    loading gachmons controllers, removed unnecessary files

--- app/router-config.js
+++ app/router-config.js
@@ -1,12 +1,11 @@
-import { ExamplesController } from "./controllers/ExamplesController.js";
-import { HomeController } from "./controllers/HomeController.js";
+import { GachamonsController } from "./controllers/GachamonsController.js";
 import { Router } from "./utils/Router.js";
 
 
 export const router = new Router([
   {
     path: '',
-    controllers: [HomeController, ExamplesController],
+    controllers: [GachamonsController],
     view: ''
   },
   {
