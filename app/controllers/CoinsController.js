66a14ba630a105d0b7c22c06cd4a0814882e65fc
Jeremy Fowler <jembyfowler@gmail.com>
2024-06-18 11:42:33 -0600

    drawing coin number to page

--- app/controllers/CoinsController.js
+++ app/controllers/CoinsController.js
@@ -1,12 +1,20 @@
+import { AppState } from "../AppState.js";
 import { coinsService } from "../services/CoinsService.js";
+import { setText } from "../utils/Writer.js";
 
 export class CoinsController {
   constructor() {
     console.log('Coins Controller loaded');
+    this.drawCoins()
   }
 
   addCoin() {
     console.log('adding coin!');
     coinsService.addCoin()
+    this.drawCoins()
+  }
+
+  drawCoins() {
+    setText('coinCount', AppState.coins)
   }
 }
