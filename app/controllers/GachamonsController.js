fc7292334c46e65e2347b963a0b72824704eb73a
Jeremy Fowler <jembyfowler@gmail.com>
2024-06-18 10:13:22 -0600

    using setHTML

--- app/controllers/GachamonsController.js
+++ app/controllers/GachamonsController.js
@@ -1,4 +1,5 @@
 import { AppState } from "../AppState.js";
+import { setHTML } from "../utils/Writer.js";
 
 export class GachamonsController {
   constructor() {
@@ -12,7 +13,9 @@ export class GachamonsController {
     console.log('Drawing the gachamons!', gachamons);
     let innerHTMLString = ''
     gachamons.forEach((gachamon) => innerHTMLString += gachamon.catalogHTMLTemplate)
-    const gachamonCatalog = document.getElementById('gachamonCatalog')
-    gachamonCatalog.innerHTML = innerHTMLString
+    // const gachamonCatalog = document.getElementById('gachamonCatalog')
+    // gachamonCatalog.innerHTML = innerHTMLString
+    // NOTE utility function you can import, that takes in an ID from your HTML, and a string of HTML, and draws to said ID
+    setHTML('gachamonCatalog', innerHTMLString)
   }
 }
